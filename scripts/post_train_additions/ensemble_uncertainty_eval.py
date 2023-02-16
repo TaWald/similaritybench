e8cbc5317183eca16fb3a00e4074eaf1fd498dff
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2023-02-16 16:12:10 +0100

    Del unused ensemble_uncertainty_eval.py

--- scripts/post_train_additions/ensemble_uncertainty_eval.py
+++
@@ -1,5 +0,0 @@
-# ToDo:
-#   0. Get the configurations that should be loaded  -- In common to robustness one
-#   1. Reload all models of the ensemble
-#   2. Evaluate the robustness
-#   3. Add some
