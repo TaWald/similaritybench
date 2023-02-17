d0e544637eef5f56e1eda4e02758a08fe09c7f07
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2023-02-17 18:46:18 +0100

    Added various metrics to multi_output_metrics.

--- rep_trans/metrics/ke_metrics.py
+++ rep_trans/metrics/ke_metrics.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import math
 import pathlib
+from dataclasses import asdict
 from dataclasses import dataclass
 from functools import cache
 from typing import Optional
@@ -48,15 +50,17 @@ class SingleOutMetrics:
     accuracy: float
     ce: float
     ece: float
-    aurc: float
+    max_softmax_aurc: float
+    mutual_info_aurc: float
 
 
 @dataclass
 class MultiOutMetrics(SingleOutMetrics):
     mean_old_acc: float
-    ensemble_acc: float
-    ensemble_aurc: float
-    ece: float
+    ensemble_accuracy: float
+    ensemble_max_softmax_aurc: float
+    ensemble_mutual_info_aurc: float
+    ensemble_ece: float
     cohens_kappa: float
     error_ratio: float
     relative_cohens_kappa: float
@@ -68,6 +72,35 @@ class RepresentationMetrics:
     corr: float | None = None
     cka: float | None = None
     rel_rep_cosine_similarity: float | None = None
+
+
+def pred_entropy(logits) -> t.Tensor:
+    """Get the mean entropy of multiple logits."""
+    k = logits.shape[1]
+    out = F.log_softmax(logits, dim=2)  # BxkxD
+    out = t.logsumexp(out, dim=1) - math.log(k)  # BxkxD --> BxD
+    ent = t.sum(-t.exp(out) * out, dim=1)  # B
+    return ent
+
+
+def exp_entropy(logits) -> t.Tensor:
+    out = F.log_softmax(logits, dim=2)  # BxkxD
+    out = t.sum(-t.exp(out) * out, dim=2)  # Bxk
+    out = t.mean(out, dim=1)
+    return out
+
+
+def mutual_bald(logits) -> t.Tensor:
+    """
+    Calculation how uncertain the model is for respective samples.
+    Also wie viel information dein Modell glaubt zu gainen, wenn dieses bekannt wäre.
+    https://arxiv.org/pdf/1803.08533.pdf
+
+    Here logits are expected to have [N_Models x NSamples x NClasses]
+    """
+    tp_logits = t.transpose(logits, dim0=1, dim1=0)
+
+    return pred_entropy(tp_logits) - exp_entropy(tp_logits)
 
 
 def single_output_metrics(
@@ -89,12 +122,14 @@ def single_output_metrics(
     ece = float(ece.detach().cpu())
 
     ce = float(cross_entropy(new_output, groundtruth).detach().cpu())
-    confidence = (t.max(new_prob, dim=-1).values).to(t.float64)  # Softmax max probability
+    max_softmax_confidence = (t.max(new_prob, dim=-1).values).to(t.float64)  # Softmax max probability
     residual = (new_y_hat_class_id == groundtruth).to(t.float64)
+    mi_confidence = mutual_bald(new_output[None, ...])
 
-    aurc_val = aurc(residual, confidence)
+    aurc_sm = aurc(residual, max_softmax_confidence)
+    aurc_mi = aurc(residual, mi_confidence)
 
-    return SingleOutMetrics(accuracy=acc, ece=ece, aurc=aurc_val, ce=ce)
+    return SingleOutMetrics(accuracy=acc, ce=ce, ece=ece, max_softmax_aurc=aurc_sm, mutual_info_aurc=aurc_mi)
 
 
 def multi_output_metrics(
@@ -107,22 +142,43 @@ def multi_output_metrics(
     """
     Calculates a variety of metrics that are based on multiple output predictions being present.
     """
+    num_classes = new_output.shape[-1]
+
     with t.no_grad():
-        num_classes = new_output.shape[-1]
         # Calculation of probabilties and predicted classes.
+        all_logits = t.concat([old_outputs[None, ...], new_output], dim=0)
+
+        # Only existing model stuff
         old_probs = F.softmax(old_outputs, dim=-1)
         old_y_hat_class_ids = t.argmax(old_probs, dim=-1)
-
+        # New model stuff:
         new_prob = F.softmax(new_output, dim=-1)
         new_y_hat_class_id = t.argmax(new_prob, dim=-1)
 
         ensemble_probs = t.mean(t.concat([old_probs, new_prob[None, ...]], dim=0), dim=0)
         ensemble_y_hat = t.argmax(ensemble_probs, dim=1)
-        # ----------- Metric calculation -----------
+
         # ---- New model accuracy
-        acc = t.mean(new_y_hat_class_id == groundtruth, dtype=t.float)
-        acc = float(acc.detach().cpu())
-        new_aurc = aurc(new_y_hat_class_id == groundtruth, t.max(new_prob, dim=-1))
+        single_metrics = single_output_metrics(new_output, groundtruth)
+
+        # ----------- Ensemble Uncertainty: -----------
+        max_softmax_confidence = t.max(ensemble_probs.to(t.float64), dim=-1).values
+        mi_confidence = mutual_bald(all_logits.to(t.float64))
+
+        residual = (ensemble_y_hat == groundtruth).to(t.float64)
+        ensemble_ms_aurc = aurc(residual, max_softmax_confidence)
+        ensemble_mi_aurc = aurc(residual, mi_confidence)
+
+        # ----------- Ensemble Calibration: -----------
+        ensemble_ece = calibration_error(
+            ensemble_probs, groundtruth, task="multiclass", n_bins=15, num_classes=num_classes
+        )
+        ensemble_ece = float(ensemble_ece.detach().cpu())
+
+        # ---- Ensemble Accuracy
+        ensemble_acc = t.mean(ensemble_y_hat == groundtruth, dtype=t.float)
+        ensemble_acc = float(ensemble_acc.detach().cpu())
+
         # ---- Old mean accuracy
         old_acc = t.mean(t.stack([t.mean(tmp_y == groundtruth, dtype=t.float) for tmp_y in old_y_hat_class_ids]))
         mean_old_acc = float(old_acc.detach().cpu())
@@ -133,36 +189,21 @@ def multi_output_metrics(
         ]
         mean_error_ratio = float(np.mean(all_error_ratios))
 
-        # ---- Ensemble Accuracy
-        ensemble_acc = t.mean(ensemble_y_hat == groundtruth, dtype=t.float)
-        ensemble_acc = float(ensemble_acc.detach().cpu())
-        ensemble_aurc = aurc(
-            (ensemble_y_hat == groundtruth).to(t.float64), (t.max(ensemble_probs, dim=-1).values).to(t.float64)
-        )
-
-        # ---- Calibration Error
-        ece = calibration_error(new_prob, groundtruth, task="multiclass", n_bins=15, num_classes=num_classes)
-        ece = float(ece.detach().cpu())
-        # ensemble_entropy = t.sum((-ensemble_probs * t.log(ensemble_probs)), dim=1)
-        # ap = average_precision(preds=ensemble_probs, target=gt)
-        # true_entropy = t.mean(ensemble_entropy[true_false])
-        # false_entropy = t.mean(ensemble_entropy[~true_false])
-        # outputs["ensemble_uncertainty"] = (false_entropy - true_entropy).detach()
         # ---- Cohens Kappa
         cohens_kappas = [binary_cohens_kappa(new_y_hat_class_id, y, groundtruth) for y in old_y_hat_class_ids]
         cohens_kappa = float(t.mean(t.stack(cohens_kappas)).detach().cpu())
 
         # ---- Relative Cohens Kappa
-        baseline_cc = look_up_baseline_cohens_kappa(acc, dataset, architecture)
+        baseline_cc = look_up_baseline_cohens_kappa(single_metrics.accuracy, dataset, architecture)
         relative_cohens_kappa = cohens_kappa - baseline_cc
 
     return MultiOutMetrics(
-        accuracy=acc,
-        aurc=new_aurc,
+        **asdict(single_metrics),
         mean_old_acc=mean_old_acc,
-        ensemble_acc=ensemble_acc,
-        ensemble_aurc=ensemble_aurc,
-        ece=ece,
+        ensemble_accuracy=ensemble_acc,
+        ensemble_ece=ensemble_ece,
+        ensemble_max_softmax_aurc=ensemble_ms_aurc,
+        ensemble_mutual_info_aurc=ensemble_mi_aurc,
         error_ratio=mean_error_ratio,
         cohens_kappa=cohens_kappa,
         relative_cohens_kappa=relative_cohens_kappa,
