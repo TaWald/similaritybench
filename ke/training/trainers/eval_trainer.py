655d788c647efe890d45cc7d10e48caaa625c2de
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2023-03-24 19:59:54 +0100

    Adding temporary measure performance method which is supposed to allow already trained methods to get evaluated (again)

--- ke/training/trainers/eval_trainer.py
+++ ke/training/trainers/eval_trainer.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 from augmented_cifar.scripts.get_dataloaders import get_augmented_cifar100_test_dataloader
 from augmented_cifar.scripts.get_dataloaders import get_augmented_cifar10_test_dataloader
+from ke.data.cifar10_dm import CIFAR10DataModule
 from ke.training.ke_train_modules.EvaluationLightningModule import EvaluationLightningModule
 from ke.util import data_structs as ds
 from ke.util import file_io
@@ -52,6 +53,51 @@ class EvalTrainer:
             "num_workers": self.num_workers,
             "persistent_workers": True,
         }
+
+    def measure_performance(self):
+        """
+        Measures the generalization of the model by evaluating it on an augmented version of the test set.
+
+        """
+        if self.model_infos[0].dataset == "CIFAR10":
+            dataloaders = CIFAR10DataModule().test_dataloader(ds.Augmentation.VAL)
+        elif self.model_infos[0].dataset == "CIFAR100":
+            dataloaders = get_augmented_cifar100_test_dataloader(self.dataset_path, self.test_kwargs)
+        else:
+            raise ValueError(
+                f"Trying to measure generalization of unknown dataset! Got {self.model_infos[0].dataset}"
+            )
+
+        trainer = Trainer(
+            enable_checkpointing=False,
+            max_epochs=None,
+            accelerator="gpu",
+            devices=1,
+            precision=32,
+            default_root_dir=None,
+            enable_progress_bar=False,
+            logger=False,
+            profiler=None,
+        )
+        self.model.cuda()
+        self.model.eval()
+        self.model.clear_outputs = False
+
+        n_models = len(self.model.models)
+        all_results = [{} for _ in range(n_models)]
+        for dl in dataloaders:
+            trainer.validate(self.model, dl.dataloader)
+            final_metrics = self.model.all_metrics
+            for i in range(n_models):
+                if dl.name in all_results[i].keys():
+                    all_results[i][dl.name].update({str(dl.value): final_metrics[i]})
+                else:
+                    all_results[i][dl.name] = {str(dl.value): final_metrics[i]}
+
+        for i in range(n_models):
+            robustness_result = scalarize_robustness(all_results[i])
+            robustness_result.update({"specific_values": all_results[i]})
+            file_io.save(robustness_result, self.model.infos[i].path_ckpt_root, nc.GNRLZ_OUT_RESULTS)
 
     def measure_generalization(self):
         """
