d33db0935302b6aa8abedaf39d8b935205085c88
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2023-03-28 14:51:58 +0200

    Added new functions that inform user of the status of a training run from its info file.

--- ke/util/status_check.py
+++ ke/util/status_check.py
@@ -5,58 +5,6 @@ from pathlib import Path
 from ke.util import name_conventions as nc
 
 
-def is_calibrated(trained_model_data_root_path: Path) -> bool:
-    """Returns true if model finished calibration already.
-    Checks if the calib.json exists in the data_root_directory.
-
-    :param trained_model_data_root_path: Data root directory of the model
-    """
-    calib_file = trained_model_data_root_path / nc.CALIB_TMPLT
-    return calib_file.exists()
-
-
-def is_trained(trained_model_data_root_path: Path) -> bool:
-    """Returns true if model finished calibration already.
-    Checks if the output.json exists in the data_root_directory.
-
-    :param trained_model_data_root_path: Data root directory of the model
-    """
-    output_file = trained_model_data_root_path / nc.OUTPUT_TMPLT
-    return output_file.exists()
-
-
-def has_checkpoint(trained_model_ckpt_root_path: Path) -> bool:
-    """Returns true if model has the final checkpoint already.
-    Checks if the final.ckpt exists in the ckpt directory.
-
-    :param trained_model_ckpt_root_path: CKPT root directory of the model
-    """
-    ckpt_file = trained_model_ckpt_root_path / nc.CKPT_DIR_NAME / nc.STATIC_CKPT_NAME
-    return ckpt_file.exists()
-
-
-def measured_generalization(trained_model_ckpt_root_path: Path) -> bool:
-    """Returns true if model evaluated generalization performance.
-    Checks if the output.json exists in the data_root_directory.
-
-    :param trained_model_ckpt_root_path: Data root directory of the model
-    """
-    gen_file = trained_model_ckpt_root_path / nc.GENERALIZATION_TMPLT
-    return gen_file.exists()
-
-
-def has_predictions(trained_model_data_root_path: Path) -> bool:
-    """
-    Returns true if model has prediction logits and groundtruths of the test set already.
-    Checks if the output.json exists in the data_root_directory.
-
-    :param trained_model_data_root_path: Data root directory of the model
-    """
-    preds = trained_model_data_root_path / nc.ACTI_DIR_NAME / nc.MODEL_TEST_PD_TMPLT
-    gts = trained_model_data_root_path / nc.ACTI_DIR_NAME / nc.MODEL_TEST_GT_TMPLT
-    return preds.exists() and gts.exists()
-
-
 def model_is_finished(data_path: Path, ckpt_path) -> bool:
     """
     Verifies that the path provded contains a finished trained model.
