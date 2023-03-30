6eae984a4fac881bfc739c2d01de265a37f8f060
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2023-03-30 14:07:10 +0200

    Added check if all models are trained before trying to process as dir as it is skipped internally.

--- scripts/post_train_additions/calibrate.py
+++ scripts/post_train_additions/calibrate.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 from pathlib import Path
 
+import numpy as np
 from ke.arch.arch_loading import load_model_from_info_file
 from ke.training.ke_train_modules.CalibrationModule import Calibrator
 from ke.util import data_structs as ds
@@ -10,13 +11,20 @@ from ke.util import file_io
 from ke.util import name_conventions as nc
 from ke.util.default_params import get_default_parameters
 from ke.util.file_io import load_json
-from ke.util.file_io import output_json_has_nans
 from ke.util.file_io import save_json
 from ke.util.gpu_cluster_worker_nodes import get_workers_for_current_node
 from ke.util.load_own_objects import load_datamodule_from_info
+from ke.util.status_check import output_json_has_nans
 from scripts.post_train_additions.utils import clean_up_after_processing
 from scripts.post_train_additions.utils import should_process_a_dir
 from tqdm import tqdm
+
+
+def load_temperature_from_info(model_info: ds.FirstModelInfo):
+    if model_info.is_calibrated():
+        return load_json(model_info.path_calib_json)["val"]["temperature"]
+    else:
+        return np.NAN
 
 
 def calibrate_model(model_info: ds.FirstModelInfo) -> None:
@@ -105,6 +113,10 @@ def main():
                 raise NotImplementedError
 
             # Do the baseline model creation if it not already exists!
+        if all([ti.is_calibrated() for ti in all_training_infos]):
+            print(f"{res} is already calibrated.")
+            continue
+
         if not should_process_a_dir(res):
             continue
 
