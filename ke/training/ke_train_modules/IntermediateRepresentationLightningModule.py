33115501efa35e705c79fa5cb948dc2af295a53a
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2023-08-25 15:51:21 +0200

    Reverted IntermediateRepresentationLightningModule to pre debugging state

--- ke/training/ke_train_modules/IntermediateRepresentationLightningModule.py
+++ ke/training/ke_train_modules/IntermediateRepresentationLightningModule.py
@@ -8,13 +8,17 @@ from ke.arch.ke_architectures.feature_approximation_gradient_reversal import (
     FAGradientReversalArch,
 )
 from ke.arch.ke_architectures.feature_approximation_subtraction import FASubtractionArch
+from ke.arch.ke_architectures.single_model import SingleModel
 from ke.losses.ke_adversarial_loss import KEAdversarialTrainLoss
 from ke.losses.ke_loss import KETrainLoss
 from ke.losses.ke_sub_loss import KESubTrainLoss
 from ke.metrics.ke_metrics import multi_output_metrics
 from ke.metrics.ke_metrics import representation_metrics
+from ke.metrics.ke_metrics import single_output_metrics
 from ke.training.ke_train_modules.base_training_module import BaseLightningModule
 from ke.util import data_structs as ds
+from ke.util import name_conventions as nc
+from ke.util.file_io import save_json  # noqa
 from torch.optim.lr_scheduler import _LRScheduler as LRScheduler  # noqa
 
 
@@ -22,7 +26,7 @@ class IntermediateRepresentationLightningModule(BaseLightningModule):
     def __init__(
         self,
         model_info: ds.FirstModelInfo,
-        network: FAGradientReversalArch | FAArch | FASubtractionArch,
+        network: FAGradientReversalArch | FAArch | FASubtractionArch | SingleModel,
         save_checkpoints: bool,
         params: ds.Params,
         hparams: dict,
@@ -40,23 +44,21 @@ class IntermediateRepresentationLightningModule(BaseLightningModule):
             log=log,
         )
         self.net: FAGradientReversalArch | FAArch | FASubtractionArch = network
-        self.loss = loss
+        self.loss: KETrainLoss = loss
         self.save_approx: bool = save_approx
 
     def save_checkpoint(self):
         state_dict = self.net.get_new_model_state_dict()
-        if self.current_epoch == (self.params.num_epochs - 1):
+        if self.current_epoch >= (self.params.num_epochs - 1):
             torch.save(state_dict, self.checkpoint_path)
-        else:
-            debug_checkpoint_path = self.checkpoint_path.parent / f"{self.current_epoch}.ckpt"
-            torch.save(state_dict, debug_checkpoint_path)
-        """
+
         if self.current_epoch == 0:
             return
         if self.save_checkpoints:
             if self.current_epoch == (self.params.num_epochs - 1):
                 state_dict = self.net.get_new_model_state_dict()
                 torch.save(state_dict, self.checkpoint_path)
+
                 if self.save_approx:
                     approx_state_dict: list[tuple[dict, dict]] = self.net.get_approx_state_dict()
                     for cnt, asd in enumerate(approx_state_dict):
@@ -64,27 +66,25 @@ class IntermediateRepresentationLightningModule(BaseLightningModule):
                         ckpt = self.checkpoint_dir_path / nc.APPROX_CKPT_NAME.format(asd[0]["count"])
                         save_json(asd[0], info)
                         torch.save(asd[1], ckpt)
-            # torch.save(transfer_dict, self.checkpoint_path.parent / "transfer_layer.ckpt")
         return
-        """
 
     def load_latest_checkpoint(self):
         """Loads the latest checkpoint (only of the to be trained architecture)"""
         ckpt = torch.load(self.checkpoint_path)
-        self.net.new_arch.load_state_dict(ckpt)
+        self.net.get_new_model().load_state_dict(ckpt)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        old_inters, new_inter, old_outs, new_out = self(x)
-        ke_fwd = self.loss.forward(label=y, tbt_out=new_out)
-        # ke_fwd = self.loss.forward(
-        #     label=y,
-        #     new_out=new_out,
-        #     new_inter=new_inter,
-        #     old_inters=old_inters,
-        #     epoch_num=self.current_epoch,
-        #     global_step=self.global_step,
-        # )
+
+        old_inters, new_inter, _, new_out = self(x)
+        ke_fwd = self.loss.forward(
+            label=y,
+            new_out=new_out,
+            new_inter=new_inter,
+            old_inters=old_inters,
+            epoch_num=self.current_epoch,
+            global_step=self.global_step,
+        )
         return ke_fwd
 
     def training_epoch_end(self, outputs: list[dict]):
@@ -98,25 +98,18 @@ class IntermediateRepresentationLightningModule(BaseLightningModule):
 
     def validation_step(self, batch, batch_idx):
         x, y = batch  # ["data"], batch["label"]
+
         with torch.no_grad():
-            old_inters, new_inter, old_outs, new_out = self(x)
-            self.save_validation_values(
-                old_intermediate_reps=old_inters,
-                new_intermediate_reps=new_inter,
-                groundtruths=y,
-                old_y_hats=old_outs,
-                new_y_hat=new_out,
-                transferred_y_hats=None,
+            old_inters, new_inter, _, new_out = self(x)
+
+            return self.loss.forward(
+                label=y,
+                new_out=new_out,
+                new_inter=new_inter,
+                old_inters=old_inters,
+                epoch_num=self.current_epoch,
+                global_step=self.global_step,
             )
-            return self.loss.forward(label=y, tbt_out=new_out)
-            # return self.loss.forward(
-            #     label=y,
-            #     new_out=new_out,
-            #     new_inter=new_inter,
-            #     old_inters=old_inters,
-            #     epoch_num=self.current_epoch,
-            #     global_step=self.global_step,
-            # )
 
     def validation_epoch_end(self, outputs):
         with torch.no_grad():
@@ -132,16 +125,17 @@ class IntermediateRepresentationLightningModule(BaseLightningModule):
                 calc_cka=True,
                 calc_cos=True,
             )
+            first_out_metric = single_output_metrics(self.new_y_out, self.gts)
             out_metrics = multi_output_metrics(
                 self.new_y_out, self.old_y_outs, self.gts, self.params.dataset, self.params.architecture_name
             )
+            self.final_metrics = asdict(first_out_metric)  # Join dicts
             self.final_metrics = asdict(rep_metrics) | asdict(out_metrics)  # Join dicts
-
             tensorboard_dict = {f"metrics/{k}": v for k, v in self.final_metrics.items()}
             tensorboard_dict.update(loss_dict)
 
             prog_bar_log = {
-                "val/acc": out_metrics.accuracy,
+                "val/acc": first_out_metric.accuracy,
                 "val/corr": rep_metrics.corr,
                 "val/celu_r2": rep_metrics.celu_r2,
                 "val/CoKa": out_metrics.cohens_kappa.last_to_others_mean,
