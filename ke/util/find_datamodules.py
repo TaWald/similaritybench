f79f7b1abcfcb2db4163abb7d65c3c7225a728e5
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2023-10-30 10:03:26 +0100

    Add da parameter for datamodules, for better flexibility

--- ke/util/find_datamodules.py
+++ ke/util/find_datamodules.py
@@ -9,22 +9,22 @@ from ke.data.tiny_imagenet_dm import TinyImagenetDataModule
 from ke.util import data_structs as ds
 
 
-def get_datamodule(dataset: ds.Dataset) -> BaseDataModule:
+def get_datamodule(dataset: ds.Dataset, advanced_da: bool = True) -> BaseDataModule:
     """Returns the datamodule specified by the Dataset and the train/val/test split."""
     if dataset == ds.Dataset.CIFAR10:
-        return CIFAR10DataModule()
+        return CIFAR10DataModule(advanced_da)
     elif dataset == ds.Dataset.TEST:
         return TestDataModule()
     elif dataset == ds.Dataset.CIFAR100:
-        return CIFAR100DataModule()
+        return CIFAR100DataModule(advanced_da)
     elif dataset == ds.Dataset.IMAGENET:
-        return ImagenetDataModule()
+        return ImagenetDataModule(advanced_da)
     elif dataset == ds.Dataset.IMAGENET100:
-        return Imagenet100DataModule()
+        return Imagenet100DataModule(advanced_da)
     elif dataset == ds.Dataset.TinyIMAGENET:
-        return TinyImagenetDataModule()
+        return TinyImagenetDataModule(advanced_da)
     elif dataset == ds.Dataset.DermaMNIST:
-        return DermaMNISTDataModule()
+        return DermaMNISTDataModule(advanced_da)
     elif dataset == ds.Dataset.SPLITCIFAR100:
         raise NotImplementedError()
         # return split_cifar100_dm.SplitCIFAR100KFoldBaseDataModule(params)
