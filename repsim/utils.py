a35cd0be3d2a7f2f98e86cd02058467667625e41
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-02-08 09:35:48 +0100

    Add initial version of dataclasses to wrap representations (possibly needs to be extended later for benchmark experiment information)

--- repsim/utils.py
+++ repsim/utils.py
@@ -1,14 +1,33 @@
 import logging
 from typing import Optional
-
+from dataclasses import dataclass
+import numpy as np
 import datasets
+
+import torch
+
+from repsim.measures.utils import SHAPE_TYPE
+
+
+@dataclass
+class SingleLayerRepresentation:
+    layer_id: int
+    representation: torch.Tensor | np.ndarray
+    shape: SHAPE_TYPE
+
+
+@dataclass
+class ModelRepresentations:
+    architecture_name: str
+    model_id: int  # Additional identifier to distinguish between different models with the same name
+    representations: tuple[SingleLayerRepresentation]  # immutable to maintain ordering
+    dataset: str
+
 
 log = logging.getLogger(__name__)
 
 
-def get_dataset(
-    dataset_name: str, config: Optional[str] = None
-) -> datasets.dataset_dict.DatasetDict:
+def get_dataset(dataset_name: str, config: Optional[str] = None) -> datasets.dataset_dict.DatasetDict:
     ds = datasets.load_dataset(dataset_name, config)
     assert isinstance(ds, datasets.dataset_dict.DatasetDict)
     return ds
