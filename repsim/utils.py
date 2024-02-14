cdea1ede7477657165d4af11ce62af261b9bb60f
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-02-14 09:28:24 +0100

    Remove outdated datasets import

--- repsim/utils.py
+++ repsim/utils.py
@@ -1,8 +1,6 @@
 import logging
-from typing import Optional
 from dataclasses import dataclass
 import numpy as np
-import datasets
 
 import torch
 
@@ -25,12 +23,6 @@ class ModelRepresentations:
 
 
 log = logging.getLogger(__name__)
-
-
-def get_dataset(dataset_name: str, config: Optional[str] = None) -> datasets.dataset_dict.DatasetDict:
-    ds = datasets.load_dataset(dataset_name, config)
-    assert isinstance(ds, datasets.dataset_dict.DatasetDict)
-    return ds
 
 
 def convert_to_path_compatible(s: str) -> str:
