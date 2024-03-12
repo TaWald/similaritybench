2391cdb3474c22bac7fcb8aaeaf3511eee08c495
Tobias Schumacher <tobias.schumacher1@rwth-aachen.de>
2024-03-12 15:24:21 +0100

    removed deprecated custom gcn classes

--- graphs/gnn.py
+++ graphs/gnn.py
@@ -8,69 +8,6 @@ from torcheval.metrics.functional import multiclass_accuracy
 from tqdm import tqdm
 
 
-# class GCN(torch.nn.Module):
-#     def __init__(self, in_channels, hidden_channels, out_channels, num_layers, dropout):
-#         super(GCN, self).__init__()
-#
-#         self.convs = torch.nn.ModuleList()
-#         self.convs.append(GCNConv(in_channels, hidden_channels, cached=True))
-#         self.bns = torch.nn.ModuleList()
-#         self.bns.append(torch.nn.BatchNorm1d(hidden_channels))
-#
-#         for _ in range(num_layers - 2):
-#             self.convs.append(GCNConv(hidden_channels, hidden_channels, cached=True))
-#             self.bns.append(torch.nn.BatchNorm1d(hidden_channels))
-#         self.convs.append(GCNConv(hidden_channels, out_channels, cached=True))
-#
-#         self.dropout = dropout
-#
-#     def reset_parameters(self):
-#         for conv in self.convs:
-#             conv.reset_parameters()
-#         for bn in self.bns:
-#             bn.reset_parameters()
-#
-#     def forward(self, x, adj_t):
-#         for i, conv in enumerate(self.convs[:-1]):
-#             x = conv(x, adj_t)
-#             x = self.bns[i](x)
-#             x = func.relu(x)
-#             x = func.dropout(x, p=self.dropout, training=self.training)
-#         x = self.convs[-1](x, adj_t)
-#         return x    # .log_softmax(dim=-1)
-#
-#
-# class SAGE(torch.nn.Module):
-#     def __init__(self, in_channels, hidden_channels, out_channels, num_layers, dropout):
-#         super(SAGE, self).__init__()
-#
-#         self.convs = torch.nn.ModuleList()
-#         self.convs.append(SAGEConv(in_channels, hidden_channels))
-#         self.bns = torch.nn.ModuleList()
-#         self.bns.append(torch.nn.BatchNorm1d(hidden_channels))
-#         for _ in range(num_layers - 2):
-#             self.convs.append(SAGEConv(hidden_channels, hidden_channels))
-#             self.bns.append(torch.nn.BatchNorm1d(hidden_channels))
-#         self.convs.append(SAGEConv(hidden_channels, out_channels))
-#
-#         self.dropout = dropout
-#
-#     def reset_parameters(self):
-#         for conv in self.convs:
-#             conv.reset_parameters()
-#         for bn in self.bns:
-#             bn.reset_parameters()
-#
-#     def forward(self, x, adj_t):
-#         for i, conv in enumerate(self.convs[:-1]):
-#             x = conv(x, adj_t)
-#             x = self.bns[i](x)
-#             x = func.relu(x)
-#             x = func.dropout(x, p=self.dropout, training=self.training)
-#         x = self.convs[-1](x, adj_t)
-#         return x    # .log_softmax(dim=-1)
-
-
 def train_model(model, data, split_idx, seed: int, optimizer_params: Dict, save_path: str, b_test: bool = False):
     random.seed(seed)
     np.random.seed(seed)
