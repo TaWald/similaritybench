61986e136ef8d470c9ba6bbfce2c0d15a6d0041e
Tobias Schumacher <tobias.schumacher1@rwth-aachen.de>
2024-03-19 17:54:57 +0100

    initial inclusion of experimentStorer to label_experiment

--- repsim/benchmark/label_experiment.py
+++ repsim/benchmark/label_experiment.py
@@ -13,7 +13,8 @@ from repsim.benchmark.types_globals import GRAPH_DOMAIN
 from repsim.benchmark.types_globals import GRAPH_EXPERIMENT_SEED
 from repsim.benchmark.types_globals import LABEL_EXPERIMENT_NAME
 from repsim.benchmark.types_globals import NN_ARCHITECTURE_TYPE
-from repsim.benchmark.utils import Result
+from repsim.benchmark.utils import ExperimentStorer
+from repsim.benchmark.utils import SingleLayerRepresentation
 from repsim.measures import SYMMETRIC_MEASURES
 from scipy.stats import spearmanr
 
@@ -24,6 +25,7 @@ class LabelExperiment:
         domain: DOMAIN_TYPE,
         architecture_type: NN_ARCHITECTURE_TYPE,
         dataset: BENCHMARK_DATASET,
+        representation_dataset: BENCHMARK_DATASET | None,
         measures: list[Callable],
         **kwargs,
     ) -> None:
@@ -31,10 +33,12 @@ class LabelExperiment:
         self.measures = measures
         self.domain = domain
         self.dataset = dataset
+
+        # TODO: figure out whether this variable/distinction is really necessary
+        if representation_dataset is None:
+            self.representation_dataset = dataset
         self.architecture_type = architecture_type
         self.kwargs = kwargs
-        self.results = Result(LABEL_EXPERIMENT_NAME)
-        logger.add(self.results.basedir / "{time}.log")  # Not sure where this needs to go...
 
         self.models = [
             m
@@ -88,59 +92,114 @@ class LabelExperiment:
 
         return 1 - n_violations / n_comb_count
 
+    # def run_old(self) -> None:
+    #     """Run the experiment. Results can be accessed afterwards via the .results attribute"""
+    #
+    #     experiment_settings = EXPERIMENT_DICT[LABEL_EXPERIMENT_NAME]
+    #     setting_map = {i: setting for (i, setting) in zip(range(len(experiment_settings)), experiment_settings)}
+    #
+    #     for seed in get_args(GRAPH_EXPERIMENT_SEED):
+    #
+    #         curr_models = [model for model in self.models if model.additional_kwargs["seed_id"] == seed]
+    #         reps = dict()
+    #         for model in curr_models:
+    #             curr_setting = model.identifier
+    #             reps[curr_setting] = model.get_representation(representation_dataset=self.dataset)
+    #
+    #             start_time = time.perf_counter()
+    #             logger.info(
+    #                 f"Representation extraction for '{str(model)}' completed in {time.perf_counter() - start_time:.1f} seconds."
+    #             )
+    #
+    #         for measure in self.measures:
+    #
+    #             vals = np.full(
+    #                 shape=(len(setting_map), len(setting_map)),
+    #                 fill_value=np.nan,
+    #                 dtype=np.float32,
+    #             )
+    #
+    #             start_time = time.perf_counter()
+    #             for i in range(len(setting_map)):
+    #                 for j in range(i + 1, len(setting_map)):
+    #
+    #                     # TODO: check consensus on how which layers are extracted/considered here
+    #                     first = reps[setting_map[i]].representations[-2]
+    #                     second = reps[setting_map[j]].representations[-2]
+    #
+    #                     ret = measure(first.representation, second.representation, first.shape)
+    #                     vals[i, j] = ret
+    #                     vals[j, i] = vals[i, j]
+    #
+    #             logger.info(
+    #                 f"Comparisons for '{measure.__name__}' completed in {time.perf_counter() - start_time:.1f} seconds."
+    #             )
+    #             self.results.add(
+    #                 numpy_vals=vals,
+    #                 domain=self.domain,
+    #                 model=self.architecture_type,
+    #                 dataset=self.dataset,
+    #                 spearman_rank_corr=self._layerwise_forward_sim(vals),
+    #                 meta_accuracy=self._meta_accuracy(vals),
+    #                 seed_id=seed,
+    #                 measure=measure.__name__,
+    #             )
+    #     self.results.save()
+
     def run(self) -> None:
         """Run the experiment. Results can be accessed afterwards via the .results attribute"""
 
-        label_EXP_settings = EXPERIMENT_DICT[LABEL_EXPERIMENT_NAME]
-        setting_map = {i: setting for (i, setting) in zip(range(len(label_EXP_settings)), label_EXP_settings)}
+        all_sims = np.full(
+            (len(self.models), len(self.models), len(self.measures)),
+            fill_value=np.nan,
+            dtype=np.float32,
+        )
 
-        for seed in get_args(GRAPH_EXPERIMENT_SEED):
+        # ToDo: Pass "SingleLayerRepresentation" objects to the metrics instead of the raw representations
+        # ToDo: Issues during development
+        #  - Choice of shortcut (how many different groups, how many seeds)
+        #  - Saving and loading of representations <--
+        #   Maybe saving intermediate things would make sense?
+        #  - Saving and loading of results!
 
-            curr_models = [model for model in self.models if model.additional_kwargs["seed_id"] == seed]
-            reps = dict()
-            for model in curr_models:
-                curr_setting = model.identifier
-                reps[curr_setting] = model.get_representation(representation_dataset=self.dataset)
+        with ExperimentStorer() as storer:
+            for cnt_a, model_a in enumerate(self.models):
+                model_reps_a = model_a.get_representation(self.representation_dataset, **self.kwargs)
+                sngl_rep_a: SingleLayerRepresentation = model_reps_a.representations[-1]
 
-                start_time = time.perf_counter()
-                logger.info(
-                    f"Representation extraction for '{str(model)}' completed in {time.perf_counter() - start_time:.1f} seconds."
-                )
+                for cnt_b, model_b in enumerate(self.models):
+                    if cnt_a > cnt_b:
+                        continue
+                    model_reps_b = model_b.get_representation(self.representation_dataset, **self.kwargs)
+                    sngl_rep_b = model_reps_b.representations[-1]
 
-            for measure in self.measures:
+                    for cnt_m, measure in enumerate(self.measures):
+                        if storer.comparison_exists(sngl_rep_a, sngl_rep_b, measure.__name__):
+                            # ---------------------------- Just read from file --------------------------- #
+                            logger.info(f"Found {measure.__name__} loaded rep.")
 
-                vals = np.full(
-                    shape=(len(setting_map), len(setting_map)),
-                    fill_value=np.nan,
-                    dtype=np.float32,
-                )
+                            sim = storer.get_comp_result(sngl_rep_a, sngl_rep_b, measure.__name__)
+                        else:
+                            try:
+                                start_time = time.perf_counter()
+                                sim = measure(sngl_rep_a.representation, sngl_rep_b.representation, sngl_rep_a.shape)
+                                runtime = time.perf_counter() - start_time
+                                storer.add_results(sngl_rep_a, sngl_rep_b, measure.__name__, sim, runtime)
+                                logger.info(
+                                    f"Similarity '{sim:.02f}', measure '{measure.__name__}' comparison for '{str(model_a)}' and"
+                                    + f" '{str(model_b)}' completed in {time.perf_counter() - start_time:.1f} seconds."
+                                )
 
-                start_time = time.perf_counter()
-                for i in range(len(setting_map)):
-                    for j in range(i + 1, len(setting_map)):
+                            except Exception as e:
+                                sim = np.nan
+                                logger.error(
+                                    f"'{measure.__name__}' comparison for '{str(model_a)}' and '{str(model_b)}' failed."
+                                )
+                                logger.error(e)
 
-                        # TODO: check consensus on how which layers are extracted/considered here
-                        first = reps[setting_map[i]].representations[-2]
-                        second = reps[setting_map[j]].representations[-2]
-
-                        ret = measure(first.representation, second.representation, first.shape)
-                        vals[i, j] = ret
-                        vals[j, i] = vals[i, j]
-
-                logger.info(
-                    f"Comparisons for '{measure.__name__}' completed in {time.perf_counter() - start_time:.1f} seconds."
-                )
-                self.results.add(
-                    numpy_vals=vals,
-                    domain=self.domain,
-                    model=self.architecture_type,
-                    dataset=self.dataset,
-                    spearman_rank_corr=self._layerwise_forward_sim(vals),
-                    meta_accuracy=self._meta_accuracy(vals),
-                    seed_id=seed,
-                    measure=measure.__name__,
-                )
-        self.results.save()
+                        all_sims[cnt_a, cnt_b, cnt_m] = sim
+                        all_sims[cnt_b, cnt_a, cnt_m] = sim
+        return all_sims
 
 
 if __name__ == "__main__":
