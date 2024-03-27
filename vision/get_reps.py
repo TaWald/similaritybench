df4054f378b399b96523fdb5610d37a691fa0938
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-03-27 19:21:22 +0100

    Change vision get_reps and update the removed utils

--- vision/get_reps.py
+++ vision/get_reps.py
@@ -1,19 +1,16 @@
-import os
 from dataclasses import dataclass
 
 from loguru import logger
-from repsim.benchmark.paths import get_experiments_path
 from repsim.utils import ModelRepresentations
 from repsim.utils import SingleLayerRepresentation
 from vision.arch.abstract_acti_extr import AbsActiExtrArch
 from vision.arch.arch_loading import load_model_from_info_file
-from vision.toy_examples.rel_rep_to_jsd import extract_representations
 from vision.util import data_structs as ds
-from vision.util import default_params as dp
-from vision.util import find_architectures as fa
 from vision.util import find_datamodules as fd
 from vision.util.download import maybe_download_all_models
 from vision.util.file_io import get_vision_model_info
+from vision.util.vision_rep_extraction import extract_representations
+from vision.util.vision_rep_extraction import extract_single_layer_representations
 
 
 @dataclass
@@ -35,10 +32,60 @@ def _format_reps_appropriately(all_outs) -> list[SingleLayerRepresentation]:
             shape = "nc"
         else:
             raise ValueError(f"Unknown shape of representations: {reps.shape}")
-        all_single_layer_reps.append(
-            SingleLayerRepresentation(layer_id=int(layer_id), representation=reps, shape=shape)
-        )
+        slr = SingleLayerRepresentation(layer_id=int(layer_id))
+        slr.representation = reps
+        slr.shape = shape
+        all_single_layer_reps.append(slr)
     return all_single_layer_reps
+
+
+def get_vision_representation_on_demand(
+    architecture_name: str,
+    train_dataset: str,
+    seed_id: int,
+    setting_identifier: str | None,
+    representation_dataset: str,
+) -> ModelRepresentations:
+    """Creates Model Representations with representations that can be extracted only when needed)"""
+    if setting_identifier == "Normal":
+        model_info: ds.ModelInfo = get_vision_model_info(
+            architecture_name=architecture_name,
+            dataset=train_dataset,
+            seed_id=seed_id,
+        )
+    else:
+        model_info: ds.ModelInfo = get_vision_model_info(
+            architecture_name=architecture_name,
+            dataset=train_dataset,
+            seed_id=seed_id,
+            setting_identifier=setting_identifier,
+        )
+    model_type: AbsActiExtrArch = load_model_from_info_file(model_info, load_ckpt=True)
+    n_layers = len(model_type.hooks)
+    # ---------- Create the on-demand-callable functions for each layer ---------- #
+    all_single_layer_reps = []
+    for i in range(n_layers):
+
+        def _get_reps():
+            """Function providing the representations for a single layer on demand."""
+            loaded_model = load_model_from_info_file(model_info, load_ckpt=True)
+            datamodule = fd.get_datamodule(dataset=representation_dataset)
+            test_dataloader = datamodule.test_dataloader(batch_size=100)
+            res = extract_single_layer_representations(
+                i, loaded_model, test_dataloader, None, meta_info=True, remain_spatial=True
+            )
+            return res["reps"]
+
+        all_single_layer_reps.append(SingleLayerRepresentation(i, _extract_representation=_get_reps))
+    model_rep = ModelRepresentations(
+        setting_identifier=model_info.setting_identifier,
+        architecture_name=model_info.architecture,
+        seed_id=model_info.seed_id,
+        train_dataset=model_info.dataset,
+        representation_dataset=representation_dataset,
+        representations=tuple(all_single_layer_reps),
+    )
+    return model_rep
 
 
 def get_vision_representations(
@@ -78,7 +125,7 @@ def get_vision_representations(
     all_outs = extract_representations(
         loaded_model, test_dataloader, rel_reps=None, meta_info=True, remain_spatial=True
     )
-    all_single_layer_reps = _format_reps_appropriately(all_outs)
+    all_single_layer_reps: list[SingleLayerRepresentation] = _format_reps_appropriately(all_outs)
     model_reps = ModelRepresentations(
         setting_identifier=model_info.setting_identifier,
         architecture_name=model_info.architecture,
@@ -92,8 +139,25 @@ def get_vision_representations(
 
 if __name__ == "__main__":
     maybe_download_all_models()
-    model_info = {"architecture_name": "ResNet18", "train_dataset": "CIFAR10", "seed_id": 1}
-    reps_c10 = get_vision_representations(model_info, "CIFAR10")
-    reps_c100 = get_vision_representations(model_info, "CIFAR100")
-    print(reps_c10)
-    print(reps_c100)
+    model_info_C10 = {
+        "setting_identifier": "Normal",
+        "architecture_name": "ResNet18",
+        "train_dataset": "CIFAR10",
+        "seed_id": 1,
+        "representation_dataset": "CIFAR10",
+    }
+    model_info_C100 = {
+        "setting_identifier": "Normal",
+        "architecture_name": "ResNet18",
+        "train_dataset": "CIFAR10",
+        "seed_id": 1,
+        "representation_dataset": "CIFAR100",
+    }
+    on_demand_reps_c10 = get_vision_representation_on_demand(**model_info_C10)
+    on_demand_reps_c100 = get_vision_representation_on_demand(**model_info_C100)
+
+    alt_reps_c10 = get_vision_representations(**model_info_C10)
+    alt_reps_c100 = get_vision_representations(**model_info_C100)
+    print(on_demand_reps_c10)
+    rep = on_demand_reps_c10.representations[-1]
+    print(alt_reps_c10)
