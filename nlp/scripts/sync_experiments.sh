bdd70954593141ccb783f9b3d09ec75fca998855
Max Klabunde <max.klabunde@uni-passau.de>
2024-04-03 10:36:02 +0000

    update scripts

--- nlp/scripts/sync_experiments.sh
+++ nlp/scripts/sync_experiments.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sessionName="minio"
+sessionName="minio-down"
 
 tmux kill-session -t $sessionName
 
 tmux new -s $sessionName -d
 
 resultPath="/root/similaritybench/experiments"
-tmux send-keys -t $sessionName "mc mirror $resultPath innkube/datasets/simbench" Enter
+tmux send-keys -t $sessionName "mc mirror innkube/datasets/simbench $resultPath" Enter
--- nlp/scripts/train_sst2_memo.sh
+++ nlp/scripts/train_sst2_memo.sh
@@ -9,5 +9,9 @@ tmux send-keys -t $sessionNameTrain "clear" Enter
 
 # Train shortcut models
 for seed in {0..9}; do
-    tmux send-keys -t $sessionNameTrain "python nlp/bert_finetune.py model.seed=$seed dataset=sst2 dataset.finetuning.trainer.args.seed=$seed memorization_rate=1.0,0.75,0.5,0.25 -m" Enter
+    # Not using the 0.25 steps, because of the class distribution. Instead use 5 steps between naive guessing accuracy and 1.0.
+    # tmux send-keys -t $sessionNameTrain "python nlp/bert_finetune.py model.seed=$seed dataset=sst2 dataset.finetuning.trainer.args.seed=$seed memorization_rate=1.0,0.75,0.5,0.25 -m" Enter
+
+    # Leaving out 1.0, because it's the same as before.
+    tmux send-keys -t $sessionNameTrain "python nlp/bert_finetune.py model.seed=$seed dataset=sst2 dataset.finetuning.trainer.args.seed=$seed memorization_rate=0.889,0.779,0.668,0.558 -m" Enter
 done
