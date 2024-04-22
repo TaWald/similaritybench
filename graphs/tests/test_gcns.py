c8c6d04fbbbce0c4a4a0f259b18d6f580fe3a4f7
Tobias Schumacher <tobias.schumacher1@rwth-aachen.de>
2024-04-22 11:53:20 +0200

    make test script more flexible

--- graphs/tests/test_gcns.py
+++ graphs/tests/test_gcns.py
@@ -1,86 +1,132 @@
 import argparse
 import copy
-import os
-from typing import List
+import itertools
+import json
+from pathlib import Path
+from time import localtime
+from time import strftime
 
 import numpy as np
 import pandas as pd
 import torch
-from graphs.config import DATA_DIR
+from graphs.config import DATASET_LIST
 from graphs.config import GNN_DICT
 from graphs.config import GNN_LIST
-from graphs.config import RES_DIR
+from graphs.config import GNN_PARAMS_DICT
+from graphs.config import OPTIMIZER_PARAMS_DICT
+from graphs.config import SPLIT_IDX_TEST_KEY
+from graphs.config import TORCH_STATE_DICT_FILE_NAME_SEED
+from graphs.config import TRAIN_LOG_FILE_NAME_SEED
+from graphs.gnn import get_representations
 from graphs.gnn import train_model
-from ogb.nodeproppred import PygNodePropPredDataset
-from torch_geometric import transforms as t
+from graphs.tests.graph_trainer import GraphTrainer
+from repsim.benchmark.paths import BASE_PATH
+from repsim.benchmark.types_globals import GRAPH_ARCHITECTURE_TYPE
+from repsim.benchmark.types_globals import GRAPH_DATASET_TRAINED_ON
+from repsim.benchmark.types_globals import GRAPH_EXPERIMENT_SEED
 
 SEEDS = [1, 2, 3, 4, 5]
 
+RES_DIR = Path(BASE_PATH, "graph_tests")
 
-def benchmark_models(n_layers: int, architecture_list: List[str], gpu_id: int):
 
-    dev_str = f"cuda:{gpu_id}" if torch.cuda.is_available() else "cpu"
-    device = torch.device(dev_str)
+class GNNTester:
 
-    dataset = PygNodePropPredDataset(
-        name="ogbn-arxiv", transform=t.Compose([t.ToUndirected(), t.ToSparseTensor()]), root=DATA_DIR
-    )
+    def __init__(
+        self,
+        architecture_type: GRAPH_ARCHITECTURE_TYPE,
+        dataset_name: GRAPH_DATASET_TRAINED_ON,
+        seed: GRAPH_EXPERIMENT_SEED,
+        model_name: str,
+        device: int | str = 0,
+    ):
 
-    data, n_classes, split_idx = dataset[0], dataset.num_classes, dataset.get_idx_split()
+        self.architecture_type = architecture_type
+        self.seed = seed
+        self.dataset_name: GRAPH_DATASET_TRAINED_ON = dataset_name
+        self.data, self.n_classes, self.split_idx = GraphTrainer.get_data(self.dataset_name)
+        self.model_name = model_name
 
-    gnn_params = {
-        "num_layers": n_layers,
-        "in_channels": data.num_features,
-        "hidden_channels": 256,
-        "dropout": 0.5,
-        "out_channels": n_classes,
-        "norm": "BatchNorm",
-    }
-    gat_kwargs = {
-        "heads": 3,
-        "dropout": 0.05,
-    }
-    optimizer_params = {"epochs": 500, "lr": 0.01}
+        if isinstance(device, str):
+            self.device = torch.device(device)
+        else:
+            dev_str = f"cuda:{device}" if torch.cuda.is_available() else "cpu"
+            self.device = torch.device(dev_str)
 
-    for gnn in architecture_list:
+        self.gnn_params = copy.deepcopy(GNN_PARAMS_DICT[self.architecture_type][self.dataset_name])
+        self.gnn_params["in_channels"] = self.data.num_features
+        self.gnn_params["out_channels"] = self.n_classes
+        self.optimizer_params = OPTIMIZER_PARAMS_DICT[self.architecture_type][self.dataset_name]
 
-        test_accs = []
-        curr_params = copy.deepcopy(gnn_params)
+        model_dataset_path = RES_DIR / self.dataset_name
+        Path(model_dataset_path).mkdir(parents=True, exist_ok=True)
 
-        if gnn == "GAT":
-            for k, v in gat_kwargs.items():
-                curr_params[k] = v
-            curr_params["hidden_channels"] *= gat_kwargs["heads"]
-        for seed in SEEDS:
-            model = GNN_DICT[gnn](**curr_params)
-            benchmark_path = os.path.join(RES_DIR, "test_gnns")
-            if not os.path.isdir(benchmark_path):
-                os.mkdir(benchmark_path)
-            model_path = os.path.join(benchmark_path, gnn)
-            if not os.path.isdir(model_path):
-                os.mkdir(model_path)
-            save_path = os.path.join(model_path, f"model_torch_{gnn}_{n_layers}l_s{seed}.pt")
-            train_results, test_acc = train_model(
-                model=model,
-                data=data,
-                split_idx=split_idx,
-                device=device,
-                seed=seed,
-                optimizer_params=optimizer_params,
-                save_path=save_path,
-                b_test=True,
-            )
-            print(train_results[-1])
+        arch_path = model_dataset_path / self.architecture_type
+        Path(arch_path).mkdir(parents=True, exist_ok=True)
 
-            test_accs.append(test_acc)
+        self.model_path = arch_path / self.model_name
+        Path(self.model_path).mkdir(parents=True, exist_ok=True)
 
-            df_train = pd.DataFrame(
-                train_results, columns=["Epoch", "Loss", "Training_Accuracy", "Validation_Accuracy", "Test_Accuracy"]
-            )
-            df_train.to_csv(os.path.join(model_path, f"train_log_torch_{gnn}_{n_layers}l_s{seed}.csv"), index=False)
+        with open(self.model_path / "gnn_params.json", "w") as fp:
+            json.dump(self.gnn_params, fp)
 
-        print(f"Mean Test Accuracy for torch {gnn} is:")
-        print(np.mean(test_accs))
+        with open(self.model_path / "optimizer_params.json", "w") as fp:
+            json.dump(self.optimizer_params, fp)
+
+    def _load_model(self):
+
+        model = GNN_DICT[self.architecture_type](**self.gnn_params)
+        model_file = self.model_path / TORCH_STATE_DICT_FILE_NAME_SEED(self.seed)
+
+        print(model_file)
+        if not model_file.is_file():
+            raise FileNotFoundError(f"Model File for seed {self.seed} does not exist")
+
+        model.load_state_dict(torch.load(model_file, map_location=self.device))
+
+        return model
+
+    def train_test_model(self):
+
+        print(f"Train {self.architecture_type} on {self.dataset_name}")
+
+        model = GNN_DICT[self.architecture_type](**self.gnn_params)
+        save_path = self.model_path / TORCH_STATE_DICT_FILE_NAME_SEED(self.seed)
+
+        train_results, test_acc = train_model(
+            model=model,
+            data=self.data,
+            split_idx=self.split_idx,
+            device=self.device,
+            seed=self.seed,
+            optimizer_params=self.optimizer_params,
+            save_path=save_path,
+            b_test=True,
+        )
+        print(train_results[-1])
+
+        df_log = pd.DataFrame(
+            train_results, columns=["Epoch", "Loss", "Training_Accuracy", "Validation_Accuracy", "Test_Accuracy"]
+        )
+        df_log.to_csv(
+            self.model_path / TRAIN_LOG_FILE_NAME_SEED(self.seed),
+            index=False,
+        )
+
+        return test_acc
+
+    def get_test_representations(self):
+
+        model = self._load_model()
+
+        reps = get_representations(
+            model=model,
+            data=self.data,
+            test_idx=self.split_idx[SPLIT_IDX_TEST_KEY],
+            layer_ids=list(range(self.gnn_params["num_layers"] - 1)),
+        )
+
+        return reps
 
 
 def parse_args():
@@ -93,7 +139,10 @@ def parse_args():
 
     # Test parameters
     parser.add_argument(
-        "-a", "--architectures", type=str, nargs="+", default=GNN_LIST, choices=GNN_LIST, help="GNN type to benchmark"
+        "-a", "--architectures", type=str, nargs="+", default=["GCN"], choices=GNN_LIST, help="GNN type to benchmark"
+    )
+    parser.add_argument(
+        "-d", "--datasets", type=str, nargs="+", default=["cora"], choices=DATASET_LIST, help="Dataset to benchmark"
     )
     parser.add_argument("-n", "--n_layers", type=int, default=3, help="Number of Layers to benchmark")
 
@@ -105,4 +154,23 @@ def parse_args():
 if __name__ == "__main__":
 
     args = parse_args()
-    benchmark_models(args.n_layers, args.architectures, args.device)
+    for architecture, dataset in itertools.product(args.architectures, args.datasets):
+
+        test_accs = []
+        model_str = f"model_{strftime('%Y-%m-%d_%H-%M-%S', localtime())}"
+        for s in SEEDS:
+            print("seed is", s)
+            trainer = GNNTester(architecture_type=architecture, dataset_name=dataset, seed=s, model_name=model_str)
+            test_accuracy = trainer.train_test_model()
+            test_accs.append(test_accuracy)
+            plain_reps = trainer.get_test_representations()
+
+            all_single_layer_reps = []
+            for layer_id, rep in plain_reps.items():
+                all_single_layer_reps.append(rep)
+
+            print("Shape of test reps is:")
+            print(all_single_layer_reps[-1].shape)
+
+        print(f"Mean Test Accuracy for {architecture} on {dataset} is:")
+        print(np.mean(test_accs))
