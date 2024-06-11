bf2c235e934369d3ec32883db237b1dc59758a68
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-06-11 10:27:46 +0200

    Update in100 creation instructions

--- vision/create_in100_dataset.py
+++ vision/create_in100_dataset.py
@@ -2,18 +2,25 @@ import os
 from argparse import ArgumentParser
 from pathlib import Path
 
+from loguru import logger
 from repsim.benchmark.paths import VISION_DATA_PATH
 from vision.data.imagenet100_ds import create_IN100_datset_from_IN1k
 
 
 def main(in1k_path: str):
+    raise NotImplementedError("This function is deprecated. Check the README for more information.")
     if "RAW_DATA" in os.environ:
         dataset_path = Path(os.environ["RAW_DATA"])
     elif "data" in os.environ:
         dataset_path = Path(os.environ["data"])
     else:
         dataset_path = Path(VISION_DATA_PATH)
-    create_IN100_datset_from_IN1k(IN100_outpath=(dataset_path / "Imagenet100"), path_to_IN1k=in1k_path)
+    in100_path = dataset_path / "Imagenet100temp"
+    if in100_path.exists():
+        logger.info("IN100 dataset already exists.")
+        return
+    logger.info("Creating IN100 dataset from IN1k...")
+    create_IN100_datset_from_IN1k(in100_outpath=in100_path, path_to_in1k=in1k_path)
 
 
 if __name__ == "__main__":
