63db0b8926a60a46517bba2533bf6214495419ae
Tobias Schumacher <tobias.schumacher1@rwth-aachen.de>
2024-08-13 16:15:00 +0200

    import p-gnn code

---
+++ graphs/pgnn.py
@@ -0,0 +1,117 @@
+##
+# Code extracted from P-GNN source code at https://github.com/JiaxuanYou/P-GNN/blob/master/model.py
+##
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import init
+
+
+class Nonlinear(nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(Nonlinear, self).__init__()
+
+        self.linear1 = nn.Linear(in_channels, hidden_channels)
+        self.linear2 = nn.Linear(hidden_channels, out_channels)
+
+        self.act = nn.ReLU()
+
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.weight.data = init.xavier_uniform_(m.weight.data, gain=nn.init.calculate_gain("relu"))
+                if m.bias is not None:
+                    m.bias.data = init.constant_(m.bias.data, 0.0)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.act(x)
+        x = self.linear2(x)
+        return x
+
+
+class PGNN_layer(nn.Module):
+    def __init__(self, in_channels, out_channels, dist_trainable=True):
+        super(PGNN_layer, self).__init__()
+        self.in_channels = in_channels
+        self.dist_trainable = dist_trainable
+
+        if self.dist_trainable:
+            self.dist_compute = Nonlinear(1, out_channels, 1)
+
+        self.linear_hidden = nn.Linear(in_channels * 2, out_channels)
+        self.linear_out_position = nn.Linear(out_channels, 1)
+        self.act = nn.ReLU()
+
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.weight.data = init.xavier_uniform_(m.weight.data, gain=nn.init.calculate_gain("relu"))
+                if m.bias is not None:
+                    m.bias.data = init.constant_(m.bias.data, 0.0)
+
+    def forward(self, feature, dists_max, dists_argmax):
+        if self.dist_trainable:
+            dists_max = self.dist_compute(dists_max.unsqueeze(-1)).squeeze()
+
+        subset_features = feature[dists_argmax.flatten(), :]
+        subset_features = subset_features.reshape((dists_argmax.shape[0], dists_argmax.shape[1], feature.shape[1]))
+        messages = subset_features * dists_max.unsqueeze(-1)
+
+        self_feature = feature.unsqueeze(1).repeat(1, dists_max.shape[1], 1)
+        messages = torch.cat((messages, self_feature), dim=-1)
+
+        messages = self.linear_hidden(messages).squeeze()
+        messages = self.act(messages)  # n*m*d
+
+        out_position = self.linear_out_position(messages).squeeze(-1)  # n*m_out
+        out_structure = torch.mean(messages, dim=1)  # n*d
+
+        return out_position, out_structure
+
+
+class PGNN(torch.nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        feature_dim,
+        hidden_channels,
+        out_channels,
+        feature_pre=True,
+        num_layers=2,
+        dropout=True,
+        **kwargs
+    ):
+        super(PGNN, self).__init__()
+        self.feature_pre = feature_pre
+        self.num_layers = num_layers
+        self.dropout = dropout
+        if num_layers == 1:
+            hidden_channels = out_channels
+        if feature_pre:
+            self.linear_pre = nn.Linear(in_channels, feature_dim)
+            self.conv_first = PGNN_layer(feature_dim, hidden_channels)
+        else:
+            self.conv_first = PGNN_layer(in_channels, hidden_channels)
+        if num_layers > 1:
+            self.conv_hidden = nn.ModuleList(
+                [PGNN_layer(hidden_channels, hidden_channels) for i in range(num_layers - 2)]
+            )
+            self.conv_out = PGNN_layer(hidden_channels, out_channels)
+
+    def forward(self, data):
+        x = data.x
+        if self.feature_pre:
+            x = self.linear_pre(x)
+        x_position, x = self.conv_first(x, data.dists_max, data.dists_argmax)
+        if self.num_layers == 1:
+            return x_position
+        # x = F.relu(x) # Note: optional!
+        if self.dropout > 0:
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        for i in range(self.num_layers - 2):
+            _, x = self.conv_hidden[i](x, data.dists_max, data.dists_argmax)
+            # x = F.relu(x) # Note: optional!
+            if self.dropout > 0:
+                x = F.dropout(x, p=self.dropout, training=self.training)
+        x_position, x = self.conv_out(x, data.dists_max, data.dists_argmax)
+        x_position = F.normalize(x_position, p=2, dim=-1)
+        return x_position
